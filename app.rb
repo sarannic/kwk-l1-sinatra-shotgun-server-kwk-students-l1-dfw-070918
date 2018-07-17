f5dcc6a4f50e3092dc7b1a3bc728dc9ec71fffa4
sarannic <snickershelton11@gmail.com>
2018-07-17 16:22:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! 
+    I built this not really but ok :)"
   end
 
 end
